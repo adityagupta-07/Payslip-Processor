08db025b3b0ed0484597fb6e9b4fead260e92aa9
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-08-25 22:53:40 +0545

    Read excel file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from python_docx_replace import docx_replace
 from docx import Document
 from datetime import datetime
 import calendar
+import openpyxl
 
 def user_input():
     file_path = input("Provide file path: ")
@@ -12,13 +13,19 @@ def user_input():
     month = input(f"Current month: {calendar.month_name[current_month_num]}. \nEnter month in number to overwrite or press enter to skip: ")
     current_month_num = int(month) if month != "" else current_month_num
     return {
-        "file_path": file_path,
+        "file_path": file_path.replace('"', ''),
         "year": current_year,
         "month": calendar.month_name[current_month_num]
     }
 
-print(user_input())
+user_input = user_input()
 
 
+wb_obj = openpyxl.load_workbook(user_input["file_path"])
+sheet_obj = wb_obj.active
 
+row = sheet_obj.max_row
+column = sheet_obj.max_column
 
+print("Total Rows:", row)
+print("Total Columns:", column)
