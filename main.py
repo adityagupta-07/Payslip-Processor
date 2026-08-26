6cea5b7a6df5cf993507c456b7d477bcd03a8875
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-08-26 22:19:28 +0545

    Store-values-in-dict-with-keys

--- main.py
+++ main.py
@@ -38,14 +38,46 @@ net_salary_paid_row_numbers = row_range_per_employee_block("Net Salary Paid")
 
 employee_block = list(zip(employee_information_row_numbers, net_salary_paid_row_numbers))
 
+data_dict = {
+    "Employee ID": "",
+    "Employee Name": "",
+    "Designation": "",
+    "PAN": "",
+    "Contact Number": "",
+    "Basic Salary": "",
+    "Allowances": "",
+    "Gross Salary": "",
+    "Gross Salary as per working hours": "",
+    "SSF Contribution by Employer": "",
+    "Bonus": "",
+    "Total": "",
+    "SSF Contribution by Employer": "",
+    "SSF Contribution by Employee": "", 
+    "TDS for the month": "", 
+    "Total Deduction": "", 
+    "Net Salary Paid": "", 
+    "Account Number": "", 
+    "Bank Name": "",
+    "Branch": "",
+    "Marital Status": "",
+    "Annual Taxable Salary": "",
+    "Annual SSF deposit": "",
+    "Annual TDS Payment": "",
+    "Annual Net Salary": "",
+    "Month": "",
+    "Method": ""
+}
+
 
 for employee in employee_block:
     print(f"Employee block: {employee}") # employee = (1, 19) tuple 
     for r in range(employee[0], employee[1] + 1): # r loops from 1 to 19
-        for c in sheet[r]: # c loops through the cells in row(r), means row(1), row(2), row(3), ...
-            target_cell = c
-            if target_cell.value is not None:
-                print(target_cell.value)
+        for target_cell in sheet[r]: # target_cell loops through the cells in row(r), means row(1), row(2), row(3), ...
+            target_value = target_cell.value.strip() if isinstance(target_cell.value, str) else target_cell.value            
+            if target_value in data_dict:
+                next_cell = sheet.cell(row=r, column=(target_cell.column+1))
+                data_dict[target_value] = next_cell.value
+                
 
-
+    print(data_dict)   
 
