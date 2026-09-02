2f0965e02d219c618a74ab8be9498b174c2d2e7a
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-09-02 19:29:48 +0545

    Format threading code

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import threading
 
 def on_button_click():
     print("Task started…")
-    def long_running_task():
-        time.sleep(5) # Simulating a long task
-        print("Task finished!")
     threading.Thread(target=long_running_task).start()
 
+def long_running_task():
+    time.sleep(5) # Simulating a long task
+    print("Task finished!")
+    
 root = tk.Tk()
 
 root.title("Basic Tkinter App")
