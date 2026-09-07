67760cee9d32179d5a52a332244e6f398e4d952f
Aditya Kumar Gupta <aditya.gupta@ultratendency.com>
2026-09-07 17:44:21 +0545

    Delete all files instead of just .docx and .pdf

--- src/payslip_processor/delete_contents.py
+++ src/payslip_processor/delete_contents.py
@@ -4,6 +4,6 @@ def delete_contents(dir):
     root_dir = Path(dir)
 
     for file_path in root_dir.glob("**/*"):
-        if file_path.is_file() and file_path.suffix.lower() in (".docx", ".pdf"):
+        if file_path.is_file():
             file_path.unlink()
     return
