d1cbe89a725c8d144161bdcdd75fed875e707073
adityagupta-07 <adityagupta.exe@gmail.com>
2026-09-24 19:55:49 +0545

    Refine excel reader

--- src/excel_reader.py
+++ src/excel_reader.py
@@ -1,10 +1,16 @@
 import openpyxl
 
-def user_input():
-    file_path = input("Provide file path: ")
-    return file_path.replace('"', '')
+def get_file_path() -> str:
+    return input("Provide file path: ")
 
-def load_excel_file(input_file_path):
-    workbook = openpyxl.load_workbook(input_file_path, data_only=True)
+
+def clean_file_path(file_path: str) -> str:
+    return file_path.replace('"', "")
+
+
+def load_workbook(file_path: str) -> openpyxl.Workbook:
+    return openpyxl.load_workbook(file_path, data_only=True)
+
+
+def get_active_sheet(workbook: openpyxl.Workbook):
     return workbook.active
-
